15440ca75b14fa75101936f298b04d91377fe5b2
fhajims <jakob.arneitz@edu.fh-joanneum.at>
2024-05-09 15:08:45 +0200

    Projektdateien hinzufügen.

---
+++ Einkommensplaner/MainActivity.cs
@@ -0,0 +1,78 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+
+namespace Einkommensplaner
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        EditText incomePerHourEditText;
+        EditText workHourperDayEditText;
+        EditText taxRateEditText;
+        EditText savingRateEditText;
+
+        TextView workSummaryTextView;
+        TextView grossIncomeTextView;
+        TextView taxPayableTextView;
+        TextView annualSavingsTextView;
+        TextView spendableIncomeTextView;
+
+        Button calculateButton;
+        RelativeLayout resultLayout;
+
+
+        void ConnectViews() {
+            incomePerHourEditText = FindViewById<EditText>(Resource.Id.incomePerHourEditText);
+            workHourperDayEditText = FindViewById<EditText>(Resource.Id.workHoursEditText);
+            taxRateEditText = FindViewById<EditText>(Resource.Id.taxRateEditText);
+            savingRateEditText = FindViewById<EditText>(Resource.Id.savingsRateEditText);
+
+            workSummaryTextView = FindViewById<TextView>(Resource.Id.workSummaryTextView);
+            grossIncomeTextView = FindViewById<TextView>(Resource.Id.grossIncomeTextView);
+            taxPayableTextView = FindViewById<TextView>(Resource.Id.taxPayableTextView);
+            annualSavingsTextView = FindViewById<TextView>(Resource.Id.savingsTextView);
+            spendableIncomeTextView = FindViewById<TextView>(Resource.Id.spendableIncomeTextView);
+
+            calculateButton = FindViewById<Button>(Resource.Id.calculateButton);
+            resultLayout = FindViewById<RelativeLayout>(Resource.Id.resultLayout);
+
+            calculateButton.Click += CalculateButton_Click;
+
+
+        }
+
+        private void CalculateButton_Click(object sender, System.EventArgs e)
+        {
+            // Den Income from User fetchen
+            double incomePerHour = double.Parse(incomePerHourEditText.Text);
+            double workHoursPerDay = double.Parse(workHourperDayEditText.Text);
+            double taxRate = double.Parse(taxRateEditText.Text);
+            double savingsRate = double.Parse(savingRateEditText.Text);
+
+            double annualWorkHourSummary = workHoursPerDay * 5 * 50;
+            double annualIncome = incomePerHour * workHoursPerDay * 5 * 50;
+            double taxPayable = (taxRate / 100) * annualIncome;
+
+
+
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            ConnectViews();
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
